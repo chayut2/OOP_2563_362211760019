efc2e35dabb61fb5b9e84bfa964abe4f839ee2b5
chayut <bestchayut.2541@rmutsvmail.com>
2020-12-08 11:05:27 +0700

    Initial commit

---
+++ src/LAB1/HelloWorld.java
@@ -0,0 +1,20 @@
+package LAB1;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+
+    }//main
+
+
+
+
+
+
+
+
+
+}//Class
