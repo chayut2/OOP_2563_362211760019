f961fba0bf3fdc5ad23dd15baa8be800146d30d0
TravelMate <bestchayut.2541@rmutsvmail.com>
2021-01-04 19:35:09 +0700

    2

--- src/LAB2/Exercise_STD_Profile.java
+++ src/LAB2/Exercise_STD_Profile.java
@@ -4,26 +4,34 @@ import java.util.Scanner;
 
 public class Exercise_STD_Profile {
     public static void main (String[] args) {
-        Scanner name = new Scanner(System.in);
-        Scanner id = new Scanner(System.in);
-        Scanner age = new Scanner(System.in);
-        Scanner phone = new Scanner(System.in);
-        Scanner email = new Scanner(System.in);
+        Scanner scanner =new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String Name =scanner.nextLine();
 
-        System.out.print("ชื่อ-สกุล");
-        String l_name = name.nextLine();
+        System.out.print("Enter Std ID: ");
+        String Id =scanner.nextLine();
 
-        System.out.print("รหัสนัศึกษา");
-        String l_id = name.nextLine();
+        System.out.print("Enter age: ");
+        int age = Integer.parseInt(scanner.nextLine());
 
-        System.out.print("อายุ");
-        String l_age = name.nextLine();
+        System.out.print("Enter tel: ");
+        String tel =scanner.nextLine();
 
-        System.out.print("เบอร์โทร");
-        String l_phone = name.nextLine();
+        System.out.print("Enter Email: ");
+        String Email =scanner.nextLine();
 
-        System.out.print("อีเมลล์");
-        String l_email = name.nextLine();
-    }
+        System.out.println(Name);
+        System.out.println(Id);
+        System.out.println(age);
+        System.out.println(tel);
+        System.out.println(Email);
 
-}
+
+
+
+
+
+    }//main
+
+
+}//class
